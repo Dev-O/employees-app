956af290118e49ac3a70d1147e4ca660d24ecd4f
Olamide Ibrahim <Olamide.Ibrahim@sigma-systems.com>
2017-11-06 15:25:24 -0500

    Revert ""
    This reverts commit 7322100e080e38a348bc1c6e923e5ebc75fad1df.

--- src/main/java/com/example/employees/Employee.java
+++ src/main/java/com/example/employees/Employee.java
@@ -17,7 +17,6 @@ public class Employee {
     private String department;
     private String email;
     private static final AtomicLong counter = new AtomicLong(100);
-    private String str = null;
 
     public Employee(String name, String lastName, String birthDate, String role, String department, String email, long id) {
         this.name = name;
